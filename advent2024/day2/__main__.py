046e90181ae8795d18728eb043c8744288c2bf3c
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-02 18:50:10 +0000

    light refactoring

--- advent2024/day2/__main__.py
+++ advent2024/day2/__main__.py
@@ -5,30 +5,25 @@ from advent2024.utils import open_input
 reports = [[int(level) for level in report] for report in open_input(2)]
 
 
+def check_pair(is_going_up, x, y):
+    if is_going_up != (x < y):
+        return False
+
+    if not (0 < abs(x - y) <= 3):
+        return False
+    return True
+
+
 def is_safe(report: list[int]):
     is_going_up = report[0] < report[1]
-    for x, y in itertools.pairwise(report):
-        if is_going_up != (x < y):
-            return False
-
-        if not (0 < abs(x - y) <= 3):
-            return False
-
-    return True
+    return all(check_pair(is_going_up, x, y) for x, y in itertools.pairwise(report))
 
 
 def is_safe_with_margin_for_error(report: list[int]):
     is_going_up = report[0] < report[1]
 
-    def check_pair(x, y):
-        if is_going_up != (x < y):
-            return False
-
-        if not (0 < abs(x - y) <= 3):
-            return False
-
     for x, y in itertools.pairwise(report):
-        if not check_pair(x, y):
+        if not check_pair(is_going_up, x, y):
             return any(
                 is_safe(report[:i] + report[i + 1 :]) for i in range(len(report))
             )
@@ -37,12 +32,12 @@ def is_safe_with_margin_for_error(report: list[int]):
 
 
 def pt1():
-    print("pt1", sum(is_safe(report) for report in reports))
+    return sum(is_safe(report) for report in reports)
 
 
 def pt2():
-    print("pt2", sum(is_safe_with_margin_for_error(report) for report in reports))
+    return sum(is_safe_with_margin_for_error(report) for report in reports)
 
 
-pt1()
-pt2()
+print(pt1())  # 432
+print(pt2())  # 488
