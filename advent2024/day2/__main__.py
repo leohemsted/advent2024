d8c8ce16e7c9b0d6985d0b5d18177d10e35d5274
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-02 18:46:22 +0000

    pt2

--- advent2024/day2/__main__.py
+++ advent2024/day2/__main__.py
@@ -17,12 +17,31 @@ def is_safe(report: list[int]):
     return True
 
 
+def is_safe_with_margin_for_error(report: list[int]):
+    is_going_up = report[0] < report[1]
+
+    def check_pair(x, y):
+        if is_going_up != (x < y):
+            return False
+
+        if not (0 < abs(x - y) <= 3):
+            return False
+
+    for x, y in itertools.pairwise(report):
+        if not check_pair(x, y):
+            return any(
+                is_safe(report[:i] + report[i + 1 :]) for i in range(len(report))
+            )
+
+    return True
+
+
 def pt1():
     print("pt1", sum(is_safe(report) for report in reports))
 
 
 def pt2():
-    pass
+    print("pt2", sum(is_safe_with_margin_for_error(report) for report in reports))
 
 
 pt1()
