5c6fd8c6f8fffe88696b5f980b7f860a358b7404
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-02 18:36:49 +0000

    add day2 pt1
    already started refecktoring for pt2

---
+++ advent2024/day2/__main__.py
@@ -0,0 +1,29 @@
+import itertools
+
+from advent2024.utils import open_input
+
+reports = [[int(level) for level in report] for report in open_input(2)]
+
+
+def is_safe(report: list[int]):
+    is_going_up = report[0] < report[1]
+    for x, y in itertools.pairwise(report):
+        if is_going_up != (x < y):
+            return False
+
+        if not (0 < abs(x - y) <= 3):
+            return False
+
+    return True
+
+
+def pt1():
+    print("pt1", sum(is_safe(report) for report in reports))
+
+
+def pt2():
+    pass
+
+
+pt1()
+pt2()
