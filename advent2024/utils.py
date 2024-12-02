35a3f6d92bc8829c35310ef0fdd4c30e93c65d16
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-02 18:36:37 +0000

    open_input: optional filename

--- advent2024/utils.py
+++ advent2024/utils.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from typing import Any
 
 
-def open_input(day: int) -> Iterator[tuple[Any], None, None]:
-    path = Path.cwd() / "advent2024" / f"day{day}" / "input"
+def open_input(day: int, filename: str = "input") -> Iterator[tuple[Any], None, None]:
+    path = Path.cwd() / "advent2024" / f"day{day}" / filename
     with path.open() as f:
         while line := f.readline():
             yield tuple(line.split())
