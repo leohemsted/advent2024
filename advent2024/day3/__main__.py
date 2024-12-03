63f91afa1ad3f8630b51b8c243747a1f091d5118
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-03 12:06:14 +0000

    day3 pt1

--- advent2024/day3/__main__.py
+++ advent2024/day3/__main__.py
@@ -2,6 +2,12 @@ import re
 
 from advent2024.utils import input_raw
 
-for command in input_raw(3):
-    muls = re.findall("mul\((\d+),(\d+)\)", command)
-    print(sum(x * y for x, y in muls))
+commands = input_raw(3)
+
+
+def pt1():
+    muls = re.findall(r"mul\((\d+),(\d+)\)", commands)
+    return sum(int(x) * int(y) for x, y in muls)
+
+
+print("pt1", pt1())
