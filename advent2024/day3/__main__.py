884f9e9b4cf514f0c58dd2595d18b059bb9290d9
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-03 12:35:14 +0000

    day 3 part 2
    code got messy
    struggled with named matches - really i wanted to have one match called
    tag that could capture mul/do/don't - but then i wouldn't be able to
    assert that the do/don't inputs dont have numbers

--- advent2024/day3/__main__.py
+++ advent2024/day3/__main__.py
@@ -10,4 +10,34 @@ def pt1():
     return sum(int(x) * int(y) for x, y in muls)
 
 
+def pt2():
+    parsed_commands = re.finditer(
+        "".join(
+            [
+                r"(?>(?P<mul>mul)\((\d+),(\d+)\))",
+                r"|",
+                r"(?>(?P<do>do)\(\))",
+                r"|",
+                r"(?>(?P<dont>don't)\(\))",
+            ]
+        ),
+        commands,
+    )
+    enabled = True
+    running_total = 0
+    for command in parsed_commands:
+        vals = command.groupdict()
+        if vals["do"]:
+            enabled = True
+        elif vals["dont"]:
+            enabled = False
+        elif vals["mul"]:
+            if enabled:
+                running_total += int(command[2]) * int(command[3])
+        else:
+            breakpoint()
+    return running_total
+
+
 print("pt1", pt1())
+print("pt2", pt2())
