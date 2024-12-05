25250c220c0de080f359658acb3ef0c559069a87
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-05 12:16:22 +0000

    switch to using a comparator

--- advent2024/day5/__main__.py
+++ advent2024/day5/__main__.py
@@ -1,35 +1,40 @@
 import itertools
-from contextlib import suppress
-from dataclasses import dataclass
+from collections import defaultdict
+from functools import cmp_to_key
 
 from advent2024.utils import input_lines
 
-
-@dataclass
-class PageOrderingRule:
-    first: int
-    later: int
-
-
 input_data = input_lines(5)
-rules = [
-    PageOrderingRule(*map(int, line.strip().split("|")))
-    for line in itertools.takewhile(str.strip, input_data)
-]
-updates: list[int] = [list(map(int, line.strip().split(","))) for line in input_data]
+
+rule_dict = defaultdict(set)
+for first, later in (
+    map(int, line.strip().split("|")) for line in itertools.takewhile(bool, input_data)
+):
+    rule_dict[first].add(later)
+
+updates: list[int] = [list(map(int, line.split(","))) for line in input_data]
 
 
 def check_update(update: list[int]) -> bool:
-    for rule in rules:
-        with suppress(ValueError):
-            if f_index := update.index(rule.first):
-                if update.index(rule.later, 0, f_index):
-                    return False
-    return True
+    return sorted(update, key=cmp_to_key(is_pairing_valid)) == update
+
+
+def is_pairing_valid(first, later):
+    return later not in rule_dict[first] or -1
 
 
 def pt1():
     return sum(update[len(update) // 2] for update in updates if check_update(update))
 
 
+def pt2():
+    def middle_pages():
+        for update in updates:
+            if update != (new := sorted(update, key=cmp_to_key(is_pairing_valid))):
+                yield new[len(new) // 2]
+
+    return sum(middle_pages())
+
+
 print("pt1", pt1())
+print("pt2", pt2())
