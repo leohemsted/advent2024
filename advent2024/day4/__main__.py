f238e2497bbef9ffa33ed96f7edaea57856c4b01
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-05 09:47:46 +0000

    day 4 part 2
    code def got scrappier as i went on this one

--- advent2024/day4/__main__.py
+++ advent2024/day4/__main__.py
@@ -1,5 +1,6 @@
 import itertools
 from dataclasses import dataclass
+from typing import Literal, Self
 
 from advent2024.utils import input_lines
 
@@ -14,10 +15,21 @@ class Coordinate:
     x: int
     y: int
 
+    def intersecting_diagonals(self) -> list[Self]:
+        # exclude any non-direction coordinates
+        assert abs(self.x) == 1 and abs(self.y) == 1
+        # returns intersecting diagonals
+        return [
+            type(self)(x=self.x * -1, y=self.y),
+            type(self)(x=self.x, y=self.y * -1),
+        ]
+
 
 all_directions = [
     Coordinate(x, y) for x, y in itertools.product([-1, 0, 1], [-1, 0, 1])
 ]
+
+diagonals = [Coordinate(x, y) for x, y in itertools.product([-1, 1], [-1, 1])]
 
 
 def char_equal(x: int, y: int, char: str) -> bool:
@@ -45,4 +57,37 @@ def pt1():
     )
 
 
+def check_nearby_char(
+    x: int,
+    y: int,
+    char_to_check: str,
+    direction: Coordinate,
+    forwards_or_back: Literal[-1, 1],
+):
+    return char_equal(
+        x + (forwards_or_back * direction.x),
+        y + (forwards_or_back * direction.y),
+        char_to_check,
+    )
+
+
+def check_for_x_mas(x, y) -> bool:
+    if char_equal(x, y, "A"):
+        for direction in diagonals:
+            if check_nearby_char(x, y, "S", direction, -1) and check_nearby_char(
+                x, y, "M", direction, 1
+            ):
+                for diag in direction.intersecting_diagonals():
+                    if check_nearby_char(x, y, "S", diag, -1) and check_nearby_char(
+                        x, y, "M", diag, 1
+                    ):
+                        return True
+    return False
+
+
+def pt2():
+    return sum(check_for_x_mas(x, y) for x in range(x_total) for y in range(y_total))
+
+
 print("pt1", pt1())
+print("pt1", pt2())
