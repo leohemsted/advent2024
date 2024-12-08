667921f2a7bdbf20d0818b610cba7744e1cfe575
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-08 15:49:12 +0000

    refactor into pt1 fn

--- advent2024/day8/__main__.py
+++ advent2024/day8/__main__.py
@@ -7,17 +7,21 @@ from advent2024.utils import input_lines
 grid = Grid([list(l) for l in input_lines(8)])
 
 
-antinodes: set[Coordinate] = set()
 antennae_groups: dict[str, set[Coordinate]] = defaultdict(set)
 
 for x, y in grid:
     if (cell_contents := grid.get(x, y)) != ".":
         antennae_groups[cell_contents].add(Coordinate(x, y))
 
-for channel, antennae in antennae_groups.items():
-    for first, second in itertools.permutations(antennae, 2):
-        vector = first.diff(second)
-        antinode = first + vector
-        if grid.in_bounds(antinode):
-            antinodes.add(antinode)
-print(len(antinodes))
+
+def get_antinodes() -> set[Coordinate]:
+    antinodes = set()
+    for antennae in antennae_groups.values():
+        for first, second in itertools.permutations(antennae, 2):
+            vector = first.diff(second)
+            if grid.in_bounds(antinode := (first + vector)):
+                antinodes.add(antinode)
+    return antinodes
+
+
+print("pt1", len(get_antinodes()))
