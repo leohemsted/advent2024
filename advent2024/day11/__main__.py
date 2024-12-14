fdd45e261d6ba73eab4761d3e3e57c2c75b46f80
Leo Hemsted <leo.hemsted@digital.cabinet-office.gov.uk>
2024-12-14 17:48:45 +0000

    day 11 pt 2
    functools.cache comes to the rescue. rewriting the loop as a recusive
    function was pretty straightforward

--- advent2024/day11/__main__.py
+++ advent2024/day11/__main__.py
@@ -1,3 +1,5 @@
+import functools
+
 from advent2024.utils import input_raw
 
 input_stones = [int(x) for x in input_raw(11).split()]
@@ -16,6 +18,22 @@ def iterate_stones(stones: list[int]) -> list[int]:
     return new_stones
 
 
+@functools.cache
+def count_stones(stone: int, blinks_remaining: int) -> int:
+    if blinks_remaining == 0:
+        return 1
+
+    if stone == 0:
+        return count_stones(1, blinks_remaining - 1)
+    elif len(string := str(stone)) % 2 == 0:
+        return count_stones(
+            int(string[: len(string) // 2]), blinks_remaining - 1
+        ) + count_stones(int(string[len(string) // 2 :]), blinks_remaining - 1)
+
+    else:
+        return count_stones(stone * 2024, blinks_remaining - 1)
+
+
 def pt1():
     stones = input_stones
     for i in range(26):
@@ -23,4 +41,9 @@ def pt1():
         stones = iterate_stones(stones)
 
 
+def pt2():
+    return sum(count_stones(stone, blinks_remaining=75) for stone in input_stones)
+
+
 print("pt1", pt1())
+print("pt2", pt2())
